051caf170733f4bed5292caf1fcb609c6d2438ce
LeKhanhGithub <khanhcong1909@gmail.com>
2022-03-09 16:32:39 +0700

    AddLayout

---
+++ BTL/Controllers/AcountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL.Controllers
+{
+    public class AcountController : Controller
+    {
+        // GET: Acount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
